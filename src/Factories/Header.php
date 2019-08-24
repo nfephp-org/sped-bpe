955c79e831c3f00a7cc3a5d09921175aa4e7ddcc
Anderson Vaz <anderson@wdhouse.com.br>
2019-08-24 15:05:33 -0300

    Criado gerador do xml, assinatura e implementando envio ao sefaz

---
+++ src/Factories/Header.php
@@ -0,0 +1,28 @@
+<?php
+namespace NFePHP\BPe\Factories;
+
+/**
+ * Class responsible for the assembly of SOAPHeader, will no longer exist
+ * in version 4.0 of the SEFAZ layout.
+ * This class is valid only until 11/2017 after that date becomes OBSOLETE
+ * and if it is used it will cause communication failure.
+ */
+
+class Header
+{
+    /**
+     * Return header
+     * @param string $namespace
+     * @param string $cUF
+     * @param string $version
+     * @return string
+     */
+    public static function get($namespace, $cUF, $version)
+    {
+        return "<bpeCabecMsg "
+            . "xmlns=\"$namespace\">"
+            . "<cUF>$cUF</cUF>"
+            . "<versaoDados>$version</versaoDados>"
+            . "</bpeCabecMsg>";
+    }
+}
